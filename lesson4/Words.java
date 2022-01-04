1d2353182cf011552c5a7cdd1dba403e8c9ef4cf
Alexander Dunaev <94072258+mrDunaev@users.noreply.github.com>
2022-01-04 19:01:35 +0300

    Create Words.java

---
+++ lesson4/Words.java
@@ -0,0 +1,26 @@
+package lesson4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+
+public class Words {
+    public static void main(String[] args) {
+        ArrayList<String> words = new ArrayList<>(Arrays.asList("One","Two","Three","Four","Two","Three","Three","Four","Four","Four"));
+
+        LinkedHashSet<String> uniqueWords = new LinkedHashSet<>();
+        uniqueWords.addAll(words);
+
+        System.out.println(words);
+        System.out.println(uniqueWords);
+
+        for (String uniqueWord : uniqueWords) {
+            int count = 0;
+            for (String word : words) {
+                if (uniqueWord.equals(word))
+                    count++;
+            }
+            System.out.println(uniqueWord + ": " + count);
+        }
+    }
+}
